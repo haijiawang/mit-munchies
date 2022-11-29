a903d3460f755bbb841d7265cdb72b9eb2c33c4a
Haley Nakamura <halnak@mit.edu>
2022-11-29 13:28:49 -0500

    Implemented router and util for event concept

---
+++ server/event/router.ts
@@ -0,0 +1,232 @@
+import type {NextFunction, Request, Response} from 'express';
+import express from 'express';
+import EventCollection from './collection';
+import * as userValidator from '../user/middleware';
+import * as eventValidator from './middleware';
+import * as util from './util';
+
+const router = express.Router();
+
+/**
+ * Get all the events
+ *
+ * @name GET /api/events
+ *
+ * @return {EventResponse[]} - A list of all the events sorted in descending
+ *                      order by event start date
+ */
+/**
+ * Get events by coordinator.
+ *
+ * @name GET /api/events?coordinator=username
+ *
+ * @return {EventResponse[]} - An array of events created by user with username, coordinator
+ * @throws {400} - If coordinator is not given
+ * @throws {404} - If coordinator is not a recognized username of any user
+ *
+ */
+/**
+ * Get events occurring within a given date range.
+ *
+ * @name GET /api/events?startrange=start&endrange=end
+ *
+ * @return {EventResponse[]} - An array of events occurring within a given date range
+ * @throws {400} - If startrange is not given (if endrange is not given, assume no end date)
+ * @throws {404} - If startrange or endrange is invalid
+ * @throws {404} - If startrange is past the current date
+ *
+ */
+/**
+ * Get events by location.
+ *
+ * @name GET /api/events?location=location
+ *
+ * @return {EventResponse[]} - An array of events taking place at location
+ * @throws {400} - If location is not given
+ * @throws {404} - If location is invalid (i.e. improperly formatted as city,state(two-letter abbreviation))
+ *
+ */
+router.get(
+  '/',
+  async (req: Request, res: Response, next: NextFunction) => {
+    // Check if author query parameter was supplied
+    if (req.query.coordinator !== undefined || req.query.startrange !== undefined
+      || req.query.location !== undefined) { // any query parameter supplied
+      next();
+      return;
+    }
+    // All events
+    const allEvents = await EventCollection.findAll();
+    const response = allEvents.map(util.constructEventResponse);
+    res.status(200).json(response);
+  },
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (req.query.startrange !== undefined || req.query.location !== undefined) {
+      next();
+      return;
+    }
+    // eventValidator.isAccountExists; 
+
+    // Events by coordinator
+    const coordinatorEvents = await EventCollection.findAllByUsername(req.query.coordinator as string);
+    const response = coordinatorEvents.map(util.constructEventResponse);
+    res.status(200).json(response);
+  },
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (req.query.location !== undefined) {
+      next();
+      return;
+    }
+    // eventValidator.isValidDates; 
+
+    // Events by date
+    let end = (req.query.endrange !== undefined) ? req.query.endrange: null;
+    const dateEvents = await EventCollection.findAllByDateRange(req.query.startrange as string, end as string);
+    const response = dateEvents.map(util.constructEventResponse);
+    res.status(200).json(response);
+  },
+  [
+
+  ],
+  // Events by location
+  async (req: Request, res: Response) => {
+    // eventValidator.isValidLocation; 
+
+    const locEvents = await EventCollection.findAllByLocation(req.query.location as string);
+    const response = locEvents.map(util.constructEventResponse);
+    res.status(200).json(response);
+  },
+);
+
+/**
+ * Create a new event.
+ *
+ * @name POST /api/events
+ *
+ * @param {string} startdate - Start date of the event formatted as "yyyy-mm-dd"
+ * @param {string} enddate - End date of the event formatted as "yyyy-mm-dd"
+ * @param {string} location - The location of the event
+ * @param {string} description - The description of the items the coordinator is looking for to be donated
+ * @param {string} contact - The contact method (phone or email) by which the coordinator wants to setup donation drop offs
+ * @param {string} donationdate - Last date for accepting donations, formatted as "yyyy-mm-dd"
+ * @return {EventResponse} - The created event
+ * @throws {403} - If the user is not logged in
+ * @throws {400} - If the any of the event content is empty
+ * @throws {404} - If the startdate or enddate are invalid, or is enddate is distinctly earlier than startdate
+ * @throws {404} - If the donationdate is invalid
+ * @throws {404} - If the location state abbreviation does not exist
+ */
+router.post(
+  '/',
+  [
+    userValidator.isUserLoggedIn,
+    // eventValidator.isValidContent, 
+    // eventValidator.isValidDates,
+    // eventValidator.isValidDonationDate,
+    // eventValidaotr.isValidLocation
+  ],
+  async (req: Request, res: Response) => {
+    const userId = (req.session.userId as string) ?? ''; // Will not be an empty string since its validated in isUserLoggedIn
+    const event = await EventCollection.addOne(userId, req.body.description, req.body.startdate, req.body.enddate,
+      req.body.donactiondate, req.body.location, req.body.contact);
+
+    res.status(201).json({
+      message: 'Your event was created successfully.',
+      event: util.constructEventResponse(event)
+    });
+  }
+);
+
+/**
+ * Delete an event
+ *
+ * @name DELETE /api/events/:id
+ *
+ * @return {string} - A success message
+ * @throws {403} - If the user is not logged in
+ * @throws {403} - If the user is not the coordinator of the event
+ * @throws {404} - If the eventId is not valid
+ */
+router.delete(
+  '/:eventId?',
+  [
+    userValidator.isUserLoggedIn,
+    // eventValidator.isEventExists, 
+    // eventValidator.isValidCoordinator 
+  ],
+  async (req: Request, res: Response) => {
+    await EventCollection.deleteOne(req.params.eventId);
+    res.status(200).json({
+      message: 'Your event was deleted successfully.'
+    });
+  }
+);
+
+/**
+ * Modify an event.
+ *
+ * @name PATCH /api/events/:id
+ *
+ * @param {string} startdate - Start date of the event formatted as "yyyy-mm-dd"
+ * @param {string} enddate - End date of the event formatted as "yyyy-mm-dd"
+ * @param {string} location - The location of the event
+ * @param {string} description - The description of the items the coordinator is looking for to be donated
+ * @param {string} contact - The contact method (phone or email) by which the coordinator wants to setup donation drop offs
+ * @param {string} donationdate - Last date for accepting donations, formatted as "yyyy-mm-dd"
+ * @return {EventResponse} - The updated event
+ * @throws {403} - If the user is not logged in or is not the coordinator of the event
+ * @throws {400} - If the any of the event content is empty
+ * @throws {404} - If the startdate or enddate are invalid, or is enddate is distinctly earlier than startdate
+ * @throws {404} - If the donationdate is invalid
+ * @throws {404} - If the location state abbreviation does not exist
+ */
+/**
+ * Modify an event's status
+ *
+ * @name PATCH /api/events/:id
+ *
+ * @param {number} event - Status of the event to update
+ * @return {EventResponse} - The updated event
+ * @throws {400} - If all fields are empty
+ * @throws {403} - if the user is not logged in or not the coordinator of the event
+ * @throws {404} - If the eventId is not valid
+ */
+ router.patch(
+  '/:eventId?',
+  [
+    userValidator.isUserLoggedIn
+  ],
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (req.body.event !== undefined) {
+      next();
+      return;
+    }
+
+    // eventValidator.isValidContent; 
+    // eventValidator.isValidDates;
+    // eventValidator.isValidDonationDate;
+    // eventValidaotr.isValidLocation;
+
+    const userId = (req.session.userId as string) ?? ''; // Will not be an empty string since its validated in isUserLoggedIn
+    const event = await EventCollection.updateOne(userId, req.body.description, req.body.startdate, req.body.enddate,
+      req.body.donactiondate, req.body.location, req.body.contact);
+
+    res.status(200).json({
+      message: 'Your event was updated successfully.',
+      event: util.constructEventResponse(event)
+    });
+  },
+  async (req: Request, res: Response) => {
+    // eventValidator.isValidEventFlag; 
+
+    const userId = (req.session.userId as string) ?? ''; // Will not be an empty string since its validated in isUserLoggedIn
+    const event = await EventCollection.updateEventOne(userId, req.body.event);
+
+    res.status(200).json({
+      message: 'Your event was updated successfully.',
+      event: util.constructEventResponse(event)
+    });
+  },
+);
+
+export {router as eventRouter};
---
+++ server/event/util.ts
@@ -0,0 +1,58 @@
+import type {HydratedDocument} from 'mongoose';
+import moment from 'moment';
+import type {Event, PopulatedEvent} from '../event/model';
+
+type EventResponse = {
+  _id: string; 
+  coordinatorId: string;
+  dateCreated: string;
+  description: string;
+  startdate: string;
+  enddate: string;
+  donationdate: string;
+  location: string;
+  event: string; // TODO: Possibly need to change to number, if it can be processed by frontend
+  contact: string;
+};
+
+/**
+ * Encode a date as an unambiguous string
+ *
+ * @param {Date} date - A date object
+ * @returns {string} - formatted date as string
+ */
+const formatDate = (date: Date): string => moment(date).format('MMMM Do YYYY, h:mm:ss a');
+
+/**
+ * Transform a raw Event object from the database into an object
+ * with all the information needed by the frontend
+ *
+ * @param {HydratedDocument<Event>} event - An event
+ * @returns {EventResponse} - The event object formatted for the frontend
+ */
+const constructEventResponse = (event: HydratedDocument<Event>): EventResponse => {
+  const eventCopy: PopulatedEvent = {
+    ...event.toObject({
+      versionKey: false // Cosmetics; prevents returning of __v property
+    })
+  };
+  const {username} = eventCopy.coordinatorId;
+  delete eventCopy.coordinatorId;
+  return {
+    ...eventCopy,
+    _id: eventCopy._id.toString(),
+    coordinatorId: username,
+    dateCreated: formatDate(event.dateCreated),
+    description: eventCopy.description,
+    startdate: formatDate(eventCopy.startdate),
+    enddate: formatDate(eventCopy.enddate),
+    donationdate: formatDate(eventCopy.donationdate),
+    location: eventCopy.location, // TODO: Format if necessary (add space between city and state)
+    event: eventCopy.event.toString(),
+    contact: eventCopy.contact
+  };
+};
+
+export {
+  constructEventResponse
+};
