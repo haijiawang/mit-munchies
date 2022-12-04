77476d59769aec6b4523c3b5582e688dab46f7ae
Haijia Wang <haijiaw@mit.edu>
2022-12-04 17:15:13 -0500

    changed app font

--- client/App.vue
+++ client/App.vue
@@ -8,29 +8,34 @@
 </template>
 
 <script>
-import NavBar from '@/components/common/NavBar.vue';
+import NavBar from "@/components/common/NavBar.vue";
 
 export default {
-  name: 'App',
-  components: {NavBar},
+  name: "App",
+  components: { NavBar },
   beforeCreate() {
     // Sync stored username to current session
-    fetch('/api/users/session', {
-      credentials: 'same-origin' // Sends express-session credentials with request
-    }).then(res => res.json()).then(res => {
-      const user = res.user;
-      this.$store.commit('setUsername', user ? user.username : null);
-    });
+    fetch("/api/users/session", {
+      credentials: "same-origin", // Sends express-session credentials with request
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        const user = res.user;
+        this.$store.commit("setUsername", user ? user.username : null);
+      });
 
     // Clear alerts on page refresh
     this.$store.state.alerts = {};
-  }
+  },
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400&display=swap");
+
 * {
   box-sizing: border-box;
+  font-family: Poppins;
 }
 
 body {
@@ -47,31 +52,31 @@ main {
 }
 
 .alerts {
-    position: absolute;
-    z-index: 99;
-    bottom: 0;
-    top: 100%;
-    left: 50%;
-    transform: translate(-50%, 10%);
-    width: 100%;
-    text-align: center;
+  position: absolute;
+  z-index: 99;
+  bottom: 0;
+  top: 100%;
+  left: 50%;
+  transform: translate(-50%, 10%);
+  width: 100%;
+  text-align: center;
 }
 
 .alerts article {
-    border-radius: 5px;
-    padding: 10px 20px;
-    color: #fff;
+  border-radius: 5px;
+  padding: 10px 20px;
+  color: #fff;
 }
 
 .alerts p {
-    margin: 0;
+  margin: 0;
 }
 
 .alerts .error {
-    background-color: rgb(166, 23, 33);
+  background-color: rgb(166, 23, 33);
 }
 
 .alerts .success {
-    background-color: rgb(45, 135, 87);
+  background-color: rgb(45, 135, 87);
 }
 </style>
